15918d17a82f49a5c8386025a9d4ca943aa5e36b
Paulo Guilherme <137457833+PauloGTomaz@users.noreply.github.com>
2025-11-07 23:58:33 +0000

    Tema 4 - Nível Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    
+    // ==========================
     // Simulação da TORRE
-    
-    
-    int movimentoTorre = 5;
+    // ==========================
+    int movimentoTorre = 5; 
     printf("Movimento da TORRE:\n");
     for (int i = 1; i <= movimentoTorre; i++) {
         printf("Direita\n");
     }
 
-    printf("\n"); 
+    printf("\n");
 
     
     // Simulação do BISPO
-    
     
     int movimentoBispo = 5;
     int i = 1;
@@ -37,6 +35,31 @@ int main() {
         printf("Esquerda\n");
         j++;
     } while (j <= movimentoRainha);
+
+    printf("\n");
+
+    
+    // Simulação do CAVALO
+    
+    int casasBaixo = 2;
+    int casasEsquerda = 1;
+
+    printf("Movimento do CAVALO:\n");
+
+    // Loop externo (for): movimenta para baixo
+    for (int passoBaixo = 1; passoBaixo <= casasBaixo; passoBaixo++) {
+        printf("Baixo\n");
+
+        // Loop interno (while): após o último movimento para baixo,
+        // faz o movimento perpendicular (para a esquerda)
+        if (passoBaixo == casasBaixo) {
+            int passoEsquerda = 1;
+            while (passoEsquerda <= casasEsquerda) {
+                printf("Esquerda\n");
+                passoEsquerda++;
+            }
+        }
+    }
 
     return 0;
 }
